2a30939fecd0fb2e630486142763897697899f1b
Antonio Arce <antoarcejr@gmail.com>
2024-04-01 19:00:02 -0400

    Changed Image to temp img

--- hot-carz-dealership/src/components/features/Car_Inv_Page/CarInventoryGrid.jsx
+++ hot-carz-dealership/src/components/features/Car_Inv_Page/CarInventoryGrid.jsx
@@ -1,45 +1,94 @@
 import React, { useState, useEffect } from "react";
-import Box from "@mui/material/Box";
-import Drawer from "@mui/material/Drawer";
-import AppBar from "@mui/material/AppBar";
-import CssBaseline from "@mui/material/CssBaseline";
-import Toolbar from "@mui/material/Toolbar";
-import List from "@mui/material/List";
-import Typography from "@mui/material/Typography";
-import ListItem from "@mui/material/ListItem";
-import Card from "@mui/material/Card";
-import CardMedia from "@mui/material/CardMedia";
-import CardContent from "@mui/material/CardContent";
-import TextField from "@mui/material/TextField";
-import { Button, Menu, MenuItem } from "@mui/material";
-import { CardActions } from "@mui/material";
+import {
+  Box,
+  Drawer,
+  AppBar,
+  CssBaseline,
+  Toolbar,
+  List,
+  Typography,
+  ListItem,
+  Card,
+  CardMedia,
+  CardContent,
+  TextField,
+  Button,
+  Menu,
+  MenuItem,
+  CardActions,
+} from "@mui/material";
 
 const drawerWidth = 240;
 
 const CarInventoryGrid = () => {
+  const [makeAnchorEl, setMakeAnchorEl] = useState(null);
   const [cars, setCars] = useState([]);
+  const [carMakes, setCarMakes] = useState([]);
+  const [carModels, setCarModels] = useState([]);
+
   const [selectedCarMake, setSelectedCarMake] = useState(null);
+  const [modelAnchorEl, setModelAnchorEl] = useState(null);
   const [selectedCarModel, setSelectedCarModel] = useState(null);
 
   useEffect(() => {
     fetch("http://localhost:5000/api/vehicles")
       .then((response) => response.json())
-      .then((data) => setCars(data))
+      .then((data) => {
+        setCars(data);
+        // Extract unique car makes
+        const makes = [...new Set(data.map((car) => car.make))];
+        setCarMakes(["None", ...makes]);
+        // Extract car models for each make
+        const models = {};
+        makes.forEach((make) => {
+          const makeModels = data
+            .filter((car) => car.make === make)
+            .map((car) => car.model);
+          models[make] = ["None", ...new Set(makeModels)];
+        });
+        setCarModels(models);
+      })
       .catch((error) => console.error("Error fetching data:", error));
   }, []);
+
+  const handleOpenMakeMenu = (event) => {
+    setMakeAnchorEl(event.currentTarget);
+  };
+
+  const handleCloseMakeMenu = () => {
+    setMakeAnchorEl(null);
+  };
 
   const handleCarMakeSelect = (carMake) => {
     setSelectedCarMake(carMake === "None" ? null : carMake);
     setSelectedCarModel(null); // Clear selected model when make changes
+    handleCloseMakeMenu();
+  };
+
+  const handleOpenModelMenu = (event) => {
+    setModelAnchorEl(event.currentTarget);
+  };
+
+  const handleCloseModelMenu = () => {
+    setModelAnchorEl(null);
   };
 
   const handleCarModelSelect = (carModel) => {
     setSelectedCarModel(carModel === "None" ? null : carModel);
+    handleCloseModelMenu();
+  };
+
+  const buttonStyle = {
+    minWidth: "160px", // Set a constant minimum width for the buttons
   };
 
   return (
     <Box sx={{ display: "flex" }}>
       <CssBaseline />
+      <AppBar
+        position="fixed"
+        sx={{ zIndex: (theme) => theme.zIndex.drawer + 1 }}
+      ></AppBar>
       <Drawer
         variant="permanent"
         sx={{
@@ -54,6 +103,65 @@ const CarInventoryGrid = () => {
         <Toolbar />
         <Box sx={{ overflow: "auto" }}>
           <List>
+            {/* Years */}
+            <ListItem disablePadding>
+              <List disablePadding>
+                <ListItem>
+                  <span>Years </span>
+                </ListItem>
+                <ListItem>
+                  <TextField
+                    id="minYear-number"
+                    label="Min"
+                    type="number"
+                    size="small"
+                    InputLabelProps={{
+                      shrink: true,
+                    }}
+                  />
+                  <span className="mr-2 ml-2">to</span>
+                  <TextField
+                    id="maxYear-number"
+                    label="Max"
+                    type="number"
+                    size="small"
+                    InputLabelProps={{
+                      shrink: true,
+                    }}
+                  />
+                </ListItem>
+              </List>
+            </ListItem>
+            {/* Mileage */}
+            <ListItem disablePadding>
+              <List disablePadding>
+                <ListItem>
+                  <span>Milage </span>
+                </ListItem>
+                <ListItem>
+                  <TextField
+                    id="minMilage-number"
+                    label="Min"
+                    type="number"
+                    size="small"
+                    InputLabelProps={{
+                      shrink: true,
+                    }}
+                  />
+                  <span className="mr-2 ml-2">to</span>
+                  <TextField
+                    id="maxMilage-number"
+                    label="Max"
+                    type="number"
+                    size="small"
+                    InputLabelProps={{
+                      shrink: true,
+                    }}
+                  />
+                </ListItem>
+              </List>
+            </ListItem>
+
             {/* Select Make */}
             <ListItem>
               <List disablePadding>
@@ -64,10 +172,30 @@ const CarInventoryGrid = () => {
                   <div>
                     <Button
                       variant="contained"
-                      onClick={() => handleCarMakeSelect("None")}
+                      sx={{ backgroundColor: "red" }}
+                      onClick={handleOpenMakeMenu}
+                      aria-controls="car-make-menu"
+                      aria-haspopup="true"
+                      style={buttonStyle}
                     >
                       {selectedCarMake ? selectedCarMake : "Select Make"}
                     </Button>
+                    <Menu
+                      id="car-make-menu"
+                      anchorEl={makeAnchorEl}
+                      open={Boolean(makeAnchorEl)}
+                      onClose={handleCloseMakeMenu}
+                    >
+                      {carMakes.map((carMake, index) => (
+                        <MenuItem
+                          key={index}
+                          onClick={() => handleCarMakeSelect(carMake)}
+                        >
+                          {carMake}
+                        </MenuItem>
+                      ))}
+                    </Menu>
+
                     {selectedCarMake && <p>Selected Make: {selectedCarMake}</p>}
                   </div>
                 </ListItem>
@@ -84,15 +212,43 @@ const CarInventoryGrid = () => {
                   <div>
                     <Button
                       variant="contained"
-                      onClick={() => handleCarModelSelect("None")}
+                      sx={{ backgroundColor: "red" }}
+                      onClick={handleOpenModelMenu}
+                      aria-controls="car-model-menu"
+                      aria-haspopup="true"
                       disabled={!selectedCarMake}
+                      style={buttonStyle}
                     >
                       {selectedCarModel ? selectedCarModel : "Select Model"}
                     </Button>
+                    <Menu
+                      id="car-model-menu"
+                      anchorEl={modelAnchorEl}
+                      open={Boolean(modelAnchorEl)}
+                      onClose={handleCloseModelMenu}
+                    >
+                      {selectedCarMake &&
+                        carModels[selectedCarMake] &&
+                        carModels[selectedCarMake].map((carModel, index) => (
+                          <MenuItem
+                            key={index}
+                            onClick={() => handleCarModelSelect(carModel)}
+                          >
+                            {carModel}
+                          </MenuItem>
+                        ))}
+                    </Menu>
+
                     {selectedCarModel && (
                       <p>Selected Model: {selectedCarModel}</p>
                     )}
                   </div>
+                </ListItem>
+                <ListItem></ListItem>
+                <ListItem>
+                  <Button variant="contained" sx={{ backgroundColor: "black" }}>
+                    Apply Filter
+                  </Button>
                 </ListItem>
               </List>
             </ListItem>
@@ -101,8 +257,8 @@ const CarInventoryGrid = () => {
       </Drawer>
       <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
         <Box sx={{ display: "flex", flexWrap: "wrap" }}>
-          {cars.map((car) => (
-            <CarCard key={car.VIN_carID} car={car} />
+          {cars.map((car, index) => (
+            <CarCard key={index} car={car} />
           ))}
         </Box>
       </Box>
@@ -115,10 +271,10 @@ const CarCard = ({ car }) => {
     <Card sx={{ maxWidth: 345, margin: 2 }}>
       <CardMedia
         component="img"
-        height="200" // Set a fixed height for the image container
-        image={"../../../imgs/cars/supra.png"}
+        height="140"
+        // image={car.pictureLibraryLink}
+        image="https://github.com/TheHungryGuy/Hot-Carz-Dealership-Frontend/blob/feature/antonio/car_inv_backend/hot-carz-dealership/src/imgs/cars/supra.png?raw=true"
         alt={car.model}
-        sx={{ objectFit: "cover" }} // Maintain aspect ratio without stretching
       />
       <CardContent>
         <Typography gutterBottom variant="h5" component="div">
