3a1016f907e99bdb9ab426bc166f53346df2f527
Antonio Arce <antoarcejr@gmail.com>
2024-04-03 23:39:17 -0400

    flowbite being annoying so removed for now

--- hot-carz-dealership/tailwind.config.js
+++ hot-carz-dealership/tailwind.config.js
@@ -7,10 +7,9 @@ module.exports = {
 
     // Or if using `src` directory:
     "./src/**/*.{js,ts,jsx,tsx}",
-    "node_modules/flowbite-react/lib/esm/**/*.js",
   ],
   theme: {
     extend: {},
   },
-  plugins: [require("flowbite/plugin")],
+  plugins: [],
 };
