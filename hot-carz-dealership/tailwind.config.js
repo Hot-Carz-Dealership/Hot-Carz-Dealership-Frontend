94d3c822ec80cc6fb80d6bb5790b44f2c7311b77
Antonio Arce <antoarcejr@gmail.com>
2024-04-03 23:01:33 -0400

    Added Flowbit-react library

--- hot-carz-dealership/tailwind.config.js
+++ hot-carz-dealership/tailwind.config.js
@@ -7,9 +7,10 @@ module.exports = {
 
     // Or if using `src` directory:
     "./src/**/*.{js,ts,jsx,tsx}",
+    "node_modules/flowbite-react/lib/esm/**/*.js",
   ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [require("flowbite/plugin")],
 };
