a8f4cf09880afd1fc5c04ee104cdaac0a3526a86
Jay Santamaria <js288@njit.edu>
2024-04-20 23:56:33 -0400

    Can now see the assigned appointments properly

--- hot-carz-dealership/src/pages/TechnicianPage.js
+++ hot-carz-dealership/src/pages/TechnicianPage.js
@@ -86,263 +86,196 @@ const styles = {
     welcomeScreenButton: {
       fontSize: "1.2em",
     },
+    largeTextStyle: {
+      fontSize: "2.5em",
+    },
   };
 
-const TechnicianPage = () => {
-  const navigate = useNavigate();
+  const TechnicianPage = () => {
+    const navigate = useNavigate();
+  
+    const [user, setUser] = useState(null);
+    const [sessionId, setSessionId] = useState(null);
+    const [serviceAppointments, setServiceAppointments] = useState([]);
+    const [members, setMembers] = useState([]);
+    const [employees, setEmployees] = useState([]);
+    const [technicians, setTechnicians] = useState([]);
+    const [selectedTab, setSelectedTab] = useState(0);
+    const [showTable, setShowTable] = useState(false);
+    const [assignedAppointments, setAssignedAppointments] = useState([]);
+    const currentDate = new Date().toLocaleDateString(); 
 
-  const [user, setUser] = useState(null);
-  const [sessionId, setSessionId] = useState(null);
-  const [serviceAppointments, setServiceAppointments] = useState([]);
-  const [members, setMembers] = useState([]);
-  const [employees, setEmployees] = useState([]);
-  const [technicians, setTechnicians] = useState([]);
-  const [selectedTab, setSelectedTab] = useState(0);
-  const [showTable, setShowTable] = useState(false);
-
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const resp = await httpClient.get("//localhost:5000/@me");
-        const user = resp.data;
-
-        if (user.employeeType !== "Technician") {
-          throw new Error("Unauthorized access");
+    useEffect(() => {
+      const fetchData = async () => {
+        try {
+          const resp = await httpClient.get("//localhost:5000/@me");
+          const user = resp.data;
+  
+          if (user.employeeType !== "Technician") {
+            throw new Error("Unauthorized access");
+          }
+  
+          setUser(user);
+          setSessionId(user.employeeID);
+  
+          const appointmentsResponse = await fetch(`${BASE_URL}/api/service-appointments`);
+          const appointmentsData = await appointmentsResponse.json();
+          setServiceAppointments(appointmentsData);
+  
+          const membersResponse = await fetch(`${BASE_URL}/api/members`);
+          const membersData = await membersResponse.json();
+          setMembers(membersData);
+  
+          const employeeResponse = await fetch(`${BASE_URL}/api/employees`);
+          const employeeData = await employeeResponse.json();
+          setEmployees(employeeData);
+  
+          const technicianResponse = await fetch(`${BASE_URL}/api/employees/technicians`);
+          const techniciansData = await technicianResponse.json();
+          setTechnicians(techniciansData);
+  
+          getTechnicianAppointments();
+        } catch (error) {
+          console.log("Not Authenticated or Unauthorized");
+          navigate("/login");
         }
-
-        setUser(user);
-        setSessionId(user.employeeID);
-
-        const appointmentsResponse = await fetch(`${BASE_URL}/api/service-appointments`);
-        const appointmentsData = await appointmentsResponse.json();
-        setServiceAppointments(appointmentsData);
-
-        const membersResponse = await fetch(`${BASE_URL}/api/members`);
-        const membersData = await membersResponse.json();
-        setMembers(membersData);
-
-        const employeeResponse = await fetch(`${BASE_URL}/api/employees`);
-        const employeeData = await employeeResponse.json();
-        setEmployees(employeeData);
-
-        const technicianResponse = await fetch(`${BASE_URL}/api/employees/technicians`);
-        const techniciansData = await technicianResponse.json();
-        setTechnicians(techniciansData);
-      } catch (error) {
-        console.log("Not Authenticated or Unauthorized");
-        navigate("/login");
-      }
-    };
-
-    fetchData();
-  }, [navigate]);
-
-  const handleSelectionChange = (appointmentId) => async (event) => {
-    const selectedTechnicianId = event.target.value;
-
-    try {
-      await assignTechnician(appointmentId, selectedTechnicianId, sessionId);
-    } catch (error) {
-      console.error('Error assigning technician:', error.message);
-    }
-  };
-
-  const getMemberDetails = (memberID) => {
-    const member = members.find((member) => member.memberID === memberID);
-    return member
-      ? {
-        memberID: member.memberID,
-        first_name: member.first_name,
-        last_name: member.last_name,
-        email: member.email,
-        phone: member.phone,
-        address: member.address,
-        state: member.state,
-        zipcode: member.zipcode,
-        join_date: member.join_date,
-      }
-      : null;
-  };
-  const handleGetStarted = () => {
-    setShowTable(true);
-  };
-
-
-  const ServiceCenter = () => {
-    // Function to handle selection change in the dropdown
+      };
+  
+      fetchData();
+    }, [navigate]);
+  
     const handleSelectionChange = (appointmentId) => async (event) => {
-      console.log("APP: " + appointmentId);
-      console.log("EMPL: " + event.target.value);
-      // Get the selected technician ID from the dropdown
       const selectedTechnicianId = event.target.value;
-
-      // Call the assignTechnician function with appointment ID, selected technician ID, and session ID
+  
       try {
         await assignTechnician(appointmentId, selectedTechnicianId, sessionId);
       } catch (error) {
         console.error('Error assigning technician:', error.message);
       }
     };
-
-
-    // Function to assign a technician to an appointment
-    // Function to assign a technician to an appointment
+  
+    const getMemberDetails = (memberID) => {
+      const member = members.find((member) => member.memberID === memberID);
+      return member
+        ? {
+          memberID: member.memberID,
+          first_name: member.first_name,
+          last_name: member.last_name,
+          email: member.email,
+          phone: member.phone,
+          address: member.address,
+          state: member.state,
+          zipcode: member.zipcode,
+          join_date: member.join_date,
+        }
+        : null;
+    };
+  
+    const handleGetStarted = () => {
+      setShowTable(true);
+    };
+  
+    const getTechnicianAppointments = async () => {
+      try {
+        const response = await fetch(`${BASE_URL}/api/technician-view-service-appointments`, {
+          method: 'GET',
+          credentials: 'include' // Include cookies in the request
+        });
+        const data = await response.json();
+        setAssignedAppointments(data); // Set the fetched appointments
+      } catch (error) {
+        console.error('Error fetching technician appointments:', error);
+      }
+    };
+  
+    const TechnicianAssignedAppointments = ({ appointments }) => {
+      return (
+        <div className="table-responsive">
+          <h2 style={{ ...styles.welcomeScreenHeading, ...styles.largeTextStyle }}>Technician Assigned Appointments</h2>
+          <table className="table table-bordered table-striped">
+            <thead className="thead-dark" style={{ ...styles.welcomeScreenHeading, ...styles.largeTextStyle }}>
+              <tr>
+                <th>Appointment ID</th>
+                <th>VIN/Car ID</th>
+                <th>Appointment Date</th>
+                <th>Service Name</th>
+                <th>Status</th>
+              </tr>
+            </thead>
+            <tbody style={{ ...styles.welcomeScreenHeading, ...styles.largeTextStyle }}>
+              {appointments.map(appointment => (
+                <tr key={appointment.appointment_id}>
+                  <td>{appointment.appointment_id}</td>
+                  <td>{appointment.VIN_carID}</td>
+                  <td>{new Date(appointment.appointment_date).toLocaleString()}</td>
+                  <td>{appointment.service_name}</td>
+                  <td>{appointment.status}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      );
+    };
+  
     const assignTechnician = (appointmentId, technicianId, sessionId) => {
-      // Create a JSON object with appointment_id, employee_id, and session_id
       const data = {
         appointment_id: appointmentId,
         employee_id: technicianId,
       };
-
+  
       fetch(`${BASE_URL}/api/manager/assign-service-appointments`, {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json'
         },
-        credentials: 'include', // Include cookies in the request
+        credentials: 'include',
         body: JSON.stringify(data)
       })
-        .then(response => {
-          if (!response.ok) {
-            throw new Error('Failed to assign technician to appointment');
-          }
-          // Handle success response here if needed
-          console.log('Technician assigned successfully');
-        })
-        .catch(error => {
-          // Handle error here
-          console.error('Error assigning technician:', error.message);
-        });
-
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Failed to assign technician to appointment');
+        }
+        console.log('Technician assigned successfully');
+      })
+      .catch(error => {
+        console.error('Error assigning technician:', error.message);
+      });
     };
-
-
-    return (
-      <div className="table-responsive">
-        <h2>Service Appointments</h2>
-        <table className="table table-bordered table-striped"> {/* Added table-striped class */}
-          <thead className="thead-dark">
-            <tr>
-              <th>Appointment ID</th>
-              <th>Member ID</th>
-              <th>First Name</th>
-              <th>Last Name</th>
-              <th>Email</th>
-              <th>Phone</th>
-              <th>Appointment Date</th>
-              <th>Technician Assigned</th>
-            </tr>
-          </thead>
-          <tbody>
-            {serviceAppointments.map(appointment => (
-              <tr key={appointment.appointment_id}>
-                <td>{appointment.appointment_id}</td>
-                <td>{appointment.memberID}</td>
-                <td>{getMemberDetails(appointment.memberID)?.first_name}</td>
-                <td>{getMemberDetails(appointment.memberID)?.last_name}</td>
-                <td>{getMemberDetails(appointment.memberID)?.email}</td>
-                <td>{getMemberDetails(appointment.memberID)?.phone}</td>
-                <td>{appointment.appointment_date}</td>
-                <td>
-                  {/* Select box for technician assignment */}
-                  <select onChange={handleSelectionChange(appointment.appointment_id)}>
-                    <option value="-" key="default">-</option>
-                    {technicians.map(technician => (
-                      <option key={technician.employeeID} value={technician.employeeID}>
-                        {technician.first_name} {technician.last_name}
-                      </option>
-                    ))}
-                  </select>
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-
-        <h2>Technicians Management</h2>
-        <table className="table table-bordered table-striped"> {/* Added table-striped class */}
-          <thead className="thead-dark">
-            <tr>
-              <th>First Name</th>
-              <th>Last Name</th>
-              <th>Email</th>
-              <th>Employee ID</th>
-              <th>Assigned Appointment</th>
-            </tr>
-          </thead>
-          <tbody>
-            {technicians.map(technician => (
-              <tr key={technician.employeeID}>
-                <td>{technician.first_name}</td>
-                <td>{technician.last_name}</td>
-                <td>{technician.email}</td>
-                <td>{technician.employeeID}</td>
-                <td>{/* Assigned Appointment */}</td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
-    );
-  };
-
-  const assignTechnician = (appointmentId, technicianId, sessionId) => {
-    const data = {
-      appointment_id: appointmentId,
-      employee_id: technicianId,
-    };
-
-    fetch(`${BASE_URL}/api/manager/assign-service-appointments`, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      credentials: 'include',
-      body: JSON.stringify(data)
-    })
-    .then(response => {
-      if (!response.ok) {
-        throw new Error('Failed to assign technician to appointment');
-      }
-      console.log('Technician assigned successfully');
-    })
-    .catch(error => {
-      console.error('Error assigning technician:', error.message);
-    });
-  };
-
-  const renderTable = () => {
-    return (
+  
+    const renderTable = () => {
+      return (
         <div style={styles.tableWrapper}>
-          <ServiceCenter />
+          <TechnicianAssignedAppointments appointments={assignedAppointments} />
         </div>
       );
-  };
-
-  return (
-    <div>
-      <div style={{ marginLeft: "200px", width: "calc(100% - 200px)" }}>
-        <div className="container-fluid">
-          <div className="row justify-content-center">
-            <div className="col-lg-10 col-md-12">
-              {showTable ? (
-                renderTable()
-              ) : (
-                <div style={styles.welcomeScreen}>
-                  <h1 style={styles.welcomeScreenHeading}>Hi {user && user.first_name}.</h1>
-                  <img src="logo512.png" alt="Logo" style={{ width: "200px", margin: "0 auto" }} />
-                  <br/>
-                  <p style={styles.welcomeScreenText}>Click the button below to get started as {user && user.employeeType}.</p>
-                  <button onClick={handleGetStarted} style={styles.welcomeScreenButton} className="btn btn-primary">Get Started</button>
-                </div>
-              )}
+    };
+  
+    return (
+      <div style={{ minHeight: "80vh" }}> {/* Adjusted minHeight */}
+        <div style={{ marginLeft: "", width: "100%", paddingTop: "20vh" }}> {/* Adjusted marginLeft, width, and paddingTop */}
+          <div className="container-fluid">
+]            <div className="row justify-content-center">
+              <div className="col-lg-10 col-md-12">
+              <h1>{user && user.first_name}, today is {currentDate}. Below are your assigned appointments: </h1> 
+                {showTable ? (
+                  renderTable()
+                ) : (
+                  <div style={styles.welcomeScreen}>
+                    <h1 style={{ ...styles.welcomeScreenHeading, ...styles.largeTextStyle }}>Hi {user && user.first_name}.</h1> {/* Applied largeTextStyle */}
+                    <img src="logo512.png" alt="Logo" style={{ width: "200px", margin: "0 auto" }} />
+                    <br/>
+                    <p style={{ ...styles.welcomeScreenText, ...styles.largeTextStyle }}>Click the button below to get started as {user && user.employeeType}.</p> {/* Applied largeTextStyle */}
+                    <button onClick={handleGetStarted} style={{ ...styles.welcomeScreenButton, ...styles.largeTextStyle }} className="btn btn-primary">Get Started</button> {/* Applied largeTextStyle */}
+                  </div>
+                )}
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
-  );
-};
-
-export default TechnicianPage;
+    );
+    
+  };
+  
+  export default TechnicianPage;
