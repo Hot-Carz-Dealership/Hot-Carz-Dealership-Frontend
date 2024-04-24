efb0a08449df97692d61ca1ead6239bf91d6354d
Patrick Kleid <mastermix676@gmail.com>
2024-04-23 20:56:08 -0400

    Setting up account page and allowing of editing of accounts

--- hot-carz-dealership/src/pages/editAccount.js
+++ hot-carz-dealership/src/pages/editAccount.js
@@ -1,5 +1,9 @@
 import React from "react";
 import Button from "@mui/material/Button";
+import { useEffect, useState } from "react";
+import httpClient from "../httpClient";
+import { Link, useNavigate } from "react-router-dom";
+import { BASE_URL, FINANCE_URL } from "../utilities/constants";
 
 const EditAccount = () => {
   const styles = {
@@ -80,8 +84,74 @@ const EditAccount = () => {
     },
   };
 
-  const handleSubmit = () => {
-    // Handle submit logic here
+  const [loading, setLoading] = useState(true);
+  const navigate = useNavigate(); // Initialize useNavigate
+  const [user, setUser] = useState([]);
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const resp = await httpClient.get(`${BASE_URL}/@me`);
+        setUser(resp.data);
+        setLoading(false);
+
+      } catch (error) {
+        console.log("Not Authenticated");
+        setLoading(false);
+        // Redirect to login page if user is not authenticated
+        navigate("/login");
+      }
+    })();
+  }, [navigate]); // Add navigate to the dependency array
+
+
+  const handleSubmit = async () => {
+    var x = document.getElementById("fname");
+    var fName = x.value;
+
+    x = document.getElementById("lname");
+    var lName = x.value;
+
+    x = document.getElementById("address");
+    var address = x.value;
+
+    x = document.getElementById("city");
+    var city = x.value;
+
+    x = document.getElementById("state");
+    var state = x.value;
+
+    x = document.getElementById("zipCode");
+    var zip = x.value;
+
+    x = document.getElementById("phnNumber");
+    var phn = x.value;
+
+    x = document.getElementById("emailAddress");
+    var email = x.value;
+
+    const data = {
+      first_name: fName,
+      last_name: lName,
+      address: address,
+      city: city,
+      state: state,
+      zipcode: zip,
+      phone: phn,
+      email: email
+    }
+
+    const requestData = {
+      method: "POST",
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      credentials: 'include',
+      body: JSON.stringify(data)
+    }
+    const response = await fetch(`${BASE_URL}/api/members/update`, requestData);
+    const responseData = await response.json()
+    window.location.href = "/account";
   };
 
   return (
@@ -90,20 +160,29 @@ const EditAccount = () => {
         Edit Account Info{" "}
       </header>
       <form>
-        <label for="name">Name: </label>
-        <input type="text" id="name" name="name" />
+        <label for="fname">First Name: </label>
+        <input type="text" id="fname" name="fname" defaultValue={user.first_name} />
+        <br />
+        <label for="lname">Last Name: </label>
+        <input type="text" id="lname" name="lname" defaultValue={user.last_name} />
         <br />
         <label for="address">Address: </label>
-        <input type="text" id="address" name="address" />
+        <input type="text" id="address" name="address" defaultValue={user.address}/>
         <br />
-        <label for="phnNumber">Address: </label>
-        <input type="text" id="phnNumber" name="phnNumber" />
+        <label for="city">City: </label>
+        <input type="text" id="city" name="city" defaultValue={user.city}/>
+        <br />
+        <label for="state">State: </label>
+        <input type="text" id="state" name="state" defaultValue={user.state}/>
+        <br />
+        <label for="zipCode">Zip Code: </label>
+        <input type="text" id="zipCode" name="zipCode" defaultValue={user.zipcode}/>
+        <br />
+        <label for="phnNumber">Phone Number: </label>
+        <input type="text" id="phnNumber" name="phnNumber" defaultValue={user.phone}/>
         <br />
         <label for="emailAddress">Email Address: </label>
-        <input type="text" id="emailAddress" name="name" />
-        <br />
-        <label for="license">Drivers License Number: </label>
-        <input type="text" id="license" name="license" />
+        <input type="text" id="emailAddress" name="emailAddress" defaultValue={user.email}/>
         <br />
         <Button
           variant="contained"
