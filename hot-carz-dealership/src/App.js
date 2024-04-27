ec3c583e375f172b535f18ad33ae20617c90a051
Antonio Arce <antoarcejr@gmail.com>
2024-04-27 14:40:29 -0400

    Navbar Now stuck to the top

--- hot-carz-dealership/src/App.js
+++ hot-carz-dealership/src/App.js
@@ -2,7 +2,7 @@
 import React from "react";
 import "./index.css";
 import "./App.css";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { Link, Routes, Route } from "react-router-dom";
 
 import Homepage from "./pages/homepage";
 import Services from "./pages/services";
@@ -25,56 +25,54 @@ import ApplyFinancing from "./pages/ApplyFinancing";
 
 function App() {
   return (
-    <Router>
-      <div>
-        <header className="header">
-          <Link to="/" className="logo">
-            Hot Carz
+    <>
+      <header className="header">
+        <Link to="/" className="logo ">
+          Hot Carz
+        </Link>
+        <nav className="nav">
+          <Link to="/" className="button">
+            Home
           </Link>
-          <nav className="nav">
-            <Link to="/" className="button">
-              Home
-            </Link>
-            <Link to="/cars" className="button">
-              Cars
-            </Link>
-            <Link to="/services" className="button">
-              Services
-            </Link>
-            <Link to="/account" className="button">
-              Account
-            </Link>
-          </nav>
-        </header>
+          <Link to="/cars" className="button">
+            Cars
+          </Link>
+          <Link to="/services" className="button">
+            Services
+          </Link>
+          <Link to="/account" className="button">
+            Account
+          </Link>
+        </nav>
+      </header>
 
-        <Routes>
-          <Route path="/" element={<Homepage />} />
-          <Route path="/cars" element={<Cars />} />
-          <Route path="/cars/:id" element={<CarDetails />} />
-          <Route path="/services" element={<Services />} />
-          <Route path="/account" element={<Account />} />
-          <Route path="/logIn" element={<LogIn />} />
-          <Route path="/signup" element={<SignUp />} />
-          <Route path="/employeeLogIn" element={<EmployeeLogIn />} />
-          <Route path="/managerPage" element={<ManagerPage />} />
-          <Route path="/editAccount" element={<EditAccount />} />
-          <Route path="/TechnicianPage" element={<TechnicianPage />} />
-          <Route path="/PurchaseCheckout" element={<PurchaseCheckout />} />
-          <Route
-            path="/PurchaseConfirmation"
-            element={<PurchaseConfirmation />}
-          />
-          <Route
-            path="/create-employee-account"
-            element={<CreateEmployeeAccount />}
-          />
-          <Route path="/add-new-vehicle" element={<AddNewVehicle />} />
-          <Route path="/add-member-vehicle" element={<AddMemberVehicle />} />
-          <Route path="/bookAppt" element={<BookAppointment />} />
-          <Route path="/apply-financing" element={<ApplyFinancing />} />
-        </Routes>
-      </div>
-    </Router>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/cars" element={<Cars />} />
+        <Route path="/cars/:id" element={<CarDetails />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/account" element={<Account />} />
+        <Route path="/logIn" element={<LogIn />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/employeeLogIn" element={<EmployeeLogIn />} />
+        <Route path="/managerPage" element={<ManagerPage />} />
+        <Route path="/editAccount" element={<EditAccount />} />
+        <Route path="/TechnicianPage" element={<TechnicianPage />} />
+        <Route path="/PurchaseCheckout" element={<PurchaseCheckout />} />
+        <Route
+          path="/PurchaseConfirmation"
+          element={<PurchaseConfirmation />}
+        />
+        <Route
+          path="/create-employee-account"
+          element={<CreateEmployeeAccount />}
+        />
+        <Route path="/add-new-vehicle" element={<AddNewVehicle />} />
+        <Route path="/add-member-vehicle" element={<AddMemberVehicle />} />
+        <Route path="/bookAppt" element={<BookAppointment />} />
+        <Route path="/apply-financing" element={<ApplyFinancing />} />
+      </Routes>
+    </>
   );
 }
 
