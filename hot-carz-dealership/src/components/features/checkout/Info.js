84682d2e6f3fd92c1aae142e4a8ec43d9b6abfa2
Antonio Arce <antoarcejr@gmail.com>
2024-05-01 03:18:13 -0400

    Clear Warrnings

--- hot-carz-dealership/src/components/features/checkout/Info.js
+++ hot-carz-dealership/src/components/features/checkout/Info.js
@@ -1,5 +1,5 @@
 import * as React from "react";
-import PropTypes from "prop-types";
+// import PropTypes from "prop-types";
 
 import List from "@mui/material/List";
 import ListItem from "@mui/material/ListItem";
