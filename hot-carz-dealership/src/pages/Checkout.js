160cda5139342fd826b5b581ccb8e7d3e4f34970
Antonio Arce <antoarcejr@gmail.com>
2024-04-30 20:00:34 -0400

    changed stepper red

--- hot-carz-dealership/src/pages/Checkout.js
+++ hot-carz-dealership/src/pages/Checkout.js
@@ -1,4 +1,7 @@
 import * as React from "react";
+import PropTypes from "prop-types";
+import { styled } from "@mui/material/styles";
+import Check from "@mui/icons-material/Check";
 
 import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
@@ -34,6 +37,54 @@ function getStepContent(step) {
       throw new Error("Unknown step");
   }
 }
+const QontoStepIconRoot = styled("div")(({ theme, ownerState }) => ({
+  color: theme.palette.mode === "dark" ? theme.palette.grey[700] : "#eaeaf0",
+  display: "flex",
+  height: 22,
+  alignItems: "center",
+  ...(ownerState.active && {
+    color: "red",
+  }),
+  "& .QontoStepIcon-completedIcon": {
+    color: "red",
+    zIndex: 1,
+    fontSize: 18,
+  },
+  "& .QontoStepIcon-circle": {
+    width: 8,
+    height: 8,
+    borderRadius: "50%",
+    backgroundColor: "currentColor",
+  },
+}));
+
+function QontoStepIcon(props) {
+  const { active, completed, className } = props;
+
+  return (
+    <QontoStepIconRoot ownerState={{ active }} className={className}>
+      {completed ? (
+        <Check className="QontoStepIcon-completedIcon" />
+      ) : (
+        <div className="QontoStepIcon-circle" />
+      )}
+    </QontoStepIconRoot>
+  );
+}
+
+QontoStepIcon.propTypes = {
+  /**
+   * Whether this step is active.
+   * @default false
+   */
+  active: PropTypes.bool,
+  className: PropTypes.string,
+  /**
+   * Mark the step as completed. Is passed to child components.
+   * @default false
+   */
+  completed: PropTypes.bool,
+};
 
 export default function Checkout() {
   const mode = "light";
@@ -148,7 +199,9 @@ export default function Checkout() {
                     }}
                     key={label}
                   >
-                    <StepLabel>{label}</StepLabel>
+                    <StepLabel StepIconComponent={QontoStepIcon}>
+                      {label}
+                    </StepLabel>
                   </Step>
                 ))}
               </Stepper>
