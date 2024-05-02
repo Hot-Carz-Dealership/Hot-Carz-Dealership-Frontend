cdffaa46dbef8434f92bb64463c55ce89b1de822
Antonio Arce <antoarcejr@gmail.com>
2024-05-02 14:05:45 -0400

    idk

--- hot-carz-dealership/src/utilities/constants.jsx
+++ hot-carz-dealership/src/utilities/constants.jsx
@@ -1,6 +1,11 @@
 // constants.js
 
 //DO NOT CHANGE THESE
-export const BASE_URL = process.env.REACT_APP_BASE_URL;
-export const FINANCE_URL = process.env.REACT_APP_FINANCE_URL;
+// export const BASE_URL = process.env.REACT_APP_BASE_URL;
+// export const FINANCE_URL = process.env.REACT_APP_FINANCE_URL;
 //GO TO YOUR .env FILE TO CHANGE FROM PRODUCTION TO LOCAL URLS
+
+export const BASE_URL =
+  "http://hot-carz-dealership-backend-production.up.railway.app";
+export const FINANCE_URL =
+  "https://hot-carz-financial-service-stub-production.up.railway.app";
