dd1380e4bc12a0acf6cded44ee8e8e836a0cb251
Antonio Arce <antoarcejr@gmail.com>
2024-05-02 16:19:08 -0400

    Railways was being dumb

--- hot-carz-dealership/src/utilities/constants.jsx
+++ hot-carz-dealership/src/utilities/constants.jsx
@@ -1,11 +1,11 @@
 // constants.js
 
-//DO NOT CHANGE THESE
-// export const BASE_URL = process.env.REACT_APP_BASE_URL;
-// export const FINANCE_URL = process.env.REACT_APP_FINANCE_URL;
-//GO TO YOUR .env FILE TO CHANGE FROM PRODUCTION TO LOCAL URLS
+// UNCOMMENT THESE LINES TO TEST LOCALLY AND COMMENT OUT THE OTHERS
+export const BASE_URL = "http://localhost:5000";
+export const FINANCE_URL = "http://localhost:5001";
 
-export const BASE_URL =
-  "https://hot-carz-dealership-backend-production.up.railway.app";
-export const FINANCE_URL =
-  "https://hot-carz-financial-service-stub-production.up.railway.app";
+// export const BASE_URL =
+//   "https://hot-carz-dealership-backend-production.up.railway.app/";
+
+// export const FINANCE_URL =
+//   "https://hot-carz-financial-service-stub-production.up.railway.app/";
