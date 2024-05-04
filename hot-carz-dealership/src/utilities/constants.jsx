78b2d4e917feeb3ff6310c1d6ca36fb3370691e5
Antonio Arce <antoarcejr@gmail.com>
2024-05-03 22:36:37 -0400

    🚂🚂🚂

--- hot-carz-dealership/src/utilities/constants.jsx
+++ hot-carz-dealership/src/utilities/constants.jsx
@@ -1,11 +1,11 @@
 // constants.js
 
 // UNCOMMENT THESE LINES TO TEST LOCALLY AND COMMENT OUT THE OTHERS
-export const BASE_URL = "http://localhost:5000";
-export const FINANCE_URL = "http://localhost:5001";
+// export const BASE_URL = "http://localhost:5000";
+// export const FINANCE_URL = "http://localhost:5001";
 
-// export const BASE_URL =
-//   "https://hot-carz-dealership-backend-production.up.railway.app/";
+export const BASE_URL =
+  "https://hot-carz-dealership-backend-production.up.railway.app/";
 
-// export const FINANCE_URL =
-//   "https://hot-carz-financial-service-stub-production.up.railway.app/";
+export const FINANCE_URL =
+  "https://hot-carz-financial-service-stub-production.up.railway.app/";
