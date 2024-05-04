cd56590bb4c982884a8af7f151adb336334156de
Antonio Arce <antoarcejr@gmail.com>
2024-05-04 17:40:59 -0400

    It works kinda

--- hot-carz-dealership/src/pages/AddonsPage.jsx
+++ hot-carz-dealership/src/pages/AddonsPage.jsx
@@ -4,10 +4,26 @@ import CardContent from "@mui/material/CardContent";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
 import { BASE_URL } from "../utilities/constants";
+import ReactRouterPrompt from "react-router-prompt";
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
 
+import Modal from "@mui/material/Modal";
 const apiUrl = `${BASE_URL}/api/vehicles/add-ons`;
 const addToCartUrl = `${BASE_URL}/api/member/add_to_cart`;
 
+const styles = {
+  modal: {
+    position: "absolute",
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
+    // width: 400,
+
+    // boxShadow: 24,
+    // p: 4,
+  },
+};
 function ItemCard({ item }) {
   const { itemID, itemName, totalCost } = item;
   const [addedToCart, setAddedToCart] = useState(false);
@@ -42,35 +58,91 @@ function ItemCard({ item }) {
   };
 
   return (
-    <Card
-      sx={{ flexBasis: "calc(33.33% - 100px)", margin: "20px", minHeight: 150 }}
-    >
-      <CardContent
-        style={{ display: "flex", flexDirection: "column", height: "100%" }}
+    <>
+      <ReactRouterPrompt
+        when={true}
+        beforeConfirm={async () => {}}
+        beforeCancel={() => {}}
       >
-        <div style={{ flexGrow: 1 }}>
-          <Typography variant="h5" component="div">
-            {itemName}
-          </Typography>
-          <Typography
-            variant="body2"
-            color="text.secondary"
-            style={{ marginBottom: "auto" }}
-          >
-            Total Cost: ${totalCost}
-          </Typography>
-        </div>
-        <Button
-          onClick={handleAddToCart}
-          variant="contained"
-          color="error"
-          disabled={addedToCart}
+        {({ isActive, onConfirm, onCancel }) =>
+          isActive && (
+            // <div className="lightbox">
+            //   <div className="container">
+            //     <p>Do you really want to leave?</p>
+            //     <button type="button" onClick={onCancel}>
+            //       Cancel
+            //     </button>
+            //     <button type="submit" onClick={onConfirm}>
+            //       Ok
+            //     </button>
+            //   </div>
+            // </div>
+            <Modal
+              open={isActive}
+              onClose={onCancel}
+              aria-labelledby="simple-modal-title"
+              aria-describedby="simple-modal-description"
+            >
+              <Box sx={styles.modal}>
+                <div className="rounded-lg bg-white p-8 shadow-2xl">
+                  <h2 className="text-lg font-bold">
+                    Do you want to apply for financing?
+                  </h2>
+                  <div className="mt-4 flex gap-2">
+                    <button
+                      onClick={onConfirm}
+                      className="rounded bg-green-50 px-4 py-2 text-sm font-medium text-green-600"
+                    >
+                      Yes
+                    </button>
+                    <button
+                      onClick={onCancel}
+                      className="rounded bg-gray-50 px-4 py-2 text-sm font-medium text-gray-600"
+                    >
+                      No
+                    </button>
+                  </div>
+                </div>
+              </Box>
+            </Modal>
+          )
+        }
+      </ReactRouterPrompt>
+
+      <Card
+        sx={{
+          flexBasis: "calc(33.33% - 100px)",
+          margin: "20px",
+          minHeight: 150,
+        }}
+      >
+        <CardContent
+          style={{ display: "flex", flexDirection: "column", height: "100%" }}
         >
-          {addedToCart ? "Added" : "Add to Cart"}
-        </Button>
-        {error && <p style={{ color: "red" }}>{error}</p>}
-      </CardContent>
-    </Card>
+          <div style={{ flexGrow: 1 }}>
+            <Typography variant="h5" component="div">
+              {itemName}
+            </Typography>
+            <Typography
+              variant="body2"
+              color="text.secondary"
+              style={{ marginBottom: "auto" }}
+            >
+              Total Cost: ${totalCost}
+            </Typography>
+          </div>
+          <Button
+            onClick={handleAddToCart}
+            variant="contained"
+            color="error"
+            disabled={addedToCart}
+          >
+            {addedToCart ? "Added" : "Add to Cart"}
+          </Button>
+          {error && <p style={{ color: "red" }}>{error}</p>}
+        </CardContent>
+      </Card>
+    </>
   );
 }
 
